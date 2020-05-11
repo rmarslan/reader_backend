3addc327c50fd87214da9d7f8b2151c3ed300e8a
arslan <rmarslan>
2020-05-11 09:02:00 +0500

    add debug module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const debug = require('debug')('reader:server');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`server is started listening on port ${PORT}...`);
+  debug(`server is started listening on port ${PORT}...`);
 });
