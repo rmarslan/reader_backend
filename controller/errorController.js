e9145fd1761e4cf39a6054ec6bf678881e9d1a35
arslan <rmarslan>
2020-05-11 12:10:32 +0500

    add mongoose validation error handler

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/AppError');
+
 const sendDevelopmentError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -21,6 +23,16 @@ const sendProductionError = (err, res) => {
   }
 };
 
+const handleDbValidation = err => {
+  const message = Object.values(err.errors)
+    .map(error => {
+      return error.message;
+    })
+    .join(' .');
+
+  return new AppError(message, 400);
+};
+
 const errorController = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -28,6 +40,7 @@ const errorController = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendDevelopmentError(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    if (err.name === 'ValidationError') err = handleDbValidation(err);
     sendProductionError(err, res);
   }
 };
