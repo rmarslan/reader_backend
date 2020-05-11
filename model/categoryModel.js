4107fbf0784859fc43a549c8061751873f13887a
arslan <rmarslan>
2020-05-11 09:35:32 +0500

    add category model

---
+++ model/categoryModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const categoryModel = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'category must have a title'],
+    minlength: [3, 'category title must be at least 3 characters long'],
+    maxlength: [50, 'Category must be less than 50 characters long'],
+    trim: true,
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+});
+
+const Category = mongoose.model('Category', categoryModel);
+
+module.exports = Category;
