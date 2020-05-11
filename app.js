98d55650a701a7a6a3ab4c5b49fa2c3607e20863
arslan <rmarslan>
2020-05-11 09:12:35 +0500

    add morgan

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug')('reader:app');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  debug('morgan is enabled');
+  app.use(morgan('dev'));
+}
 
 module.exports = app;
